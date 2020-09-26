6fe848de34b5c2795240666cf578e8a24c8a257b
LuanVo <vominhluan0406@gmail>
2020-09-26 19:44:10 +0700

    first commit

---
+++ Test Suites/TS_01.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>706fbba5-145d-4d36-80e7-78e97d5f5496</testSuiteGuid>
+   <testCaseLink>
+      <guid>d66d283f-497a-4a64-9d54-e645c2d8a900</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c73d4441-bcfd-452a-8255-8fa008f2e8ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c68b27d8-d79f-4cbe-a343-ca1d9941adb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6bcb9cb-122e-4557-ada3-9507401908f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>17e87ed4-5cd6-4c86-ad6c-ad6661322aa9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
